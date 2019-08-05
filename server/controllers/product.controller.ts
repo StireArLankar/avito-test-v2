362522693ef3a438d03af8356a0e8c9ef45f91de
StireArLankar <stire.ru@gmail.com>
2019-08-05 21:07:53 +0300

    another try

--- server/controllers/product.controller.ts
+++ server/controllers/product.controller.ts
@@ -47,6 +47,8 @@ const GetAllProductsV2 = async (): Promise<IProduct[]> => {
 }
 
 const GetAllProductsV3 = async (): Promise<IProduct[]> => {
+  const temp = await Product.find()
+  console.log(temp.length)
   return Product
     .find()
     .populate({ path: 'relationships.seller', select: 'rating name' })
