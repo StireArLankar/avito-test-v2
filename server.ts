c1daec27d479616369c9bad1dabb21bb4bdba869
StireArLankar <stire.ru@gmail.com>
2019-08-05 13:17:44 +0300

    added tslint + fixed server code

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-const express = require('express')
-const next = require('next')
-import api from './api'
+import express from 'express'
+import next from 'next'
+import dotenv from 'dotenv'
+import api from './server/api'
+
+dotenv.config()
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
+app.prepare().then(() => {
   const server = express()
 
   server.use('/api', api)
@@ -15,7 +17,7 @@ app.prepare()
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
     const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
+    return app.render(req, res, actualPage, queryParams)
   })
 
   server.get('*', (req, res) => {
@@ -24,10 +26,12 @@ app.prepare()
 
   server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err
+    // tslint:disable-next-line
     console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
   })
 })
-.catch((ex:any) => {
+.catch((ex: any) => {
+  // tslint:disable-next-line
   console.error(ex.stack)
   process.exit(1)
 })
